6e8d7f22aed4a587462abcad29d8d51218f4b60c
Mayk Brito <maykbrito@gmail.com>
2017-01-17 07:39:24 +0000

    initial commit, primeiras aulas com vue.js

---
+++ app.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        title: "Contas a Pagar",
+        bills: [
+            {date_due: '20/08/2016', name: 'Conta de luz', value:70.99, done:1},
+            {date_due: '21/08/2016', name: 'Conta de água', value:55.99, done:0},
+            {date_due: '22/08/2016', name: 'Conta de telefone', value:55.99, done:0},
+            {date_due: '23/08/2016', name: 'Supermercado', value:625.99, done:0},
+            {date_due: '24/08/2016', name: 'Cartão de crédito', value:1500.99, done:0},
+            {date_due: '25/08/2016', name: 'Empréstimo', value:2000.99, done:0},
+            {date_due: '26/08/2016', name: 'Gasolina', value:25.99, done:0},
+        ]
+    },
+    computed: {
+        status: function(){
+            var count = 0;
+            for (var i in this.bills){
+                if (!this.bills[i].done){
+                    count++;
+                }
+            }
+            return !count ? "Nenhuma conta a pagar" : "Existem "+count+" a serem pagas";
+        }
+    }
+})
