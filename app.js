58ad48524516be3e5facb896f9873e890916703c
Mayk Brito <maykbrito@gmail.com>
2017-01-17 10:29:51 +0000

    inserindo e editando

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 var app = new Vue({
     el: "#app",
     data: {
+        test:"",
         title: "Contas a Pagar",
         menus: [
             {id:0, name: "Listar contas"},
             {id:1, name: "Criar contas"},
         ],
-        activedView: 0,
+        activedView: 1,
+        formType: 'insert',
+        bill: {
+            date_due:'',
+            name:'',
+            value:0,
+            done:0,
+        },
+        names: [
+            'Conta de Luz',
+            'Conta de Água',
+            'Conta de Telefone',
+            'Supermercado',
+            'Cartão de Crédito',
+            'Empréstimo',
+            'Gasolina'
+        ],
         bills: [
             {date_due: '20/08/2016', name: 'Conta de luz', value:70.99, done:1},
             {date_due: '21/08/2016', name: 'Conta de água', value:55.99, done:0},
@@ -31,6 +48,29 @@ var app = new Vue({
     methods: {
         showView: function(id) {
             this.activedView = id;
+            if (id == 1) {
+                this.formType = 'insert';
+            }
+        },
+        submit: function() {
+            if (this.formType == 'insert') {
+                this.bills.push(this.bill);
+            }
+
+            this.bill = {
+                date_due:'',
+                    name:'',
+                    value:0,
+                    done:0,
+            };
+
+            this.activedView = 0;
+
+        },
+        loadBill: function(bill) {
+            this.bill = bill;
+            this.activedView = 1;
+            this.formType = 'update';
         }
     }
 })
